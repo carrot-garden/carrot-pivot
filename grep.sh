87ba15e61d874e20a0b532a19858f0b8346fc025
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2011-05-16 08:53:54 -0500

    a

--- grep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# find static singletons
-find . -name *.java -type f -exec egrep "private static" {} \; | egrep -v "final" | egrep -v "class" | egrep -v "{"> singletons.txt
-
-
-
-
-
